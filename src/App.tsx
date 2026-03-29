15082dbc33d75b378c105652a3d3cf6fd7544d76
SilentCrouwd <chris.buchholz.lauenau@gooogle.com>
2026-03-29 14:53:18 +0200

    solve Problem with app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="relative z-50 min-h-screen bg-slate-950 text-white overflow-hidden">
       {/* Der Hintergrund-Glow (liegt jetzt GANZ hinten) */}
       <div className="absolute top-0 left-0 -z-10 h-full w-full pointer-events-none">
-        <div className="absolute top-[-10%] left-[-10%] h-[600px] w-[600px] rounded-full bg-indigo-500/20 blur-[120px]"></div>
+        <div className="absolute top-[-10%] left-[-10%] h-150 w-150 rounded-full bg-indigo-500/20 blur-[120px]"></div>
       </div>{" "}
       {/* Deine echten Inhalte */}
       <Navbar />
