1d5c8d95cfe739346db86ea9d360a4be204c7ea1
SilentCrouwd <chris.buchholz.lauenau@gooogle.com>
2026-04-04 16:30:31 +0200

    Refactor Section2 layout and enhance CSS animations

--- src/Components/Section2.tsx
+++ src/Components/Section2.tsx
@@ -1,8 +1,8 @@
 function Section2() {
   return (
-    <div className="grid grid-cols-12 max-w-6xl gap-2 mx-auto mt-50 grid-rows-3 lg:grid-rows-1 ">
-      <div className=" col-start-2 col-span-10 w-full bg-white/15 backdrop-blur-md min-h-50 rounded-3xl border border-slate-800 hover:border-indigo-800 transition row-1 lg:row-1 lg:col-start-2 lg:col-span-3"></div>
-      <div className="col-start-2 col-span-10  w-full bg-white/5backdrop-blur-md min-h-50 rounded-3xl border border-slate-800 hover:border-indigo-800 transition row-2 lg:row-1 lg:col-start-5 lg:col-span-3">
+    <div className="grid grid-cols-12 w-full gap-6 mx-auto px-4 m-10 grid-rows-3 lg:grid-rows-1 lg:w-5xl ">
+      <div className="col-start-2 col-span-10 w-full bg-white/15 backdrop-blur-md min-h-50 rounded-3xl border border-slate-800 hover:border-indigo-800 transition lg:row-start-1 lg:col-start-1 lg:col-span-3"></div>
+      <div className="col-start-2 col-span-10 w-full bg-white/5 backdrop-blur-md min-h-50 rounded-3xl border border-slate-800 hover:border-indigo-800 transition lg:row-start-1 lg:col-start-4 lg:col-span-6">
         <p className="p-10 text-2xl tracking-tight align-middle">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
           culpa minima natus ad non officia dicta dolore corrupti ipsam dolorem,
@@ -10,7 +10,7 @@ function Section2() {
           dolores!
         </p>
       </div>
-      <div className="col-start-2 col-span-10  w-full bg-white/15 backdrop-blur-md min-h-50 rounded-3xl border border-slate-800 hover:border-indigo-800 transition row-3 lg:row-1 lg:col-start-8 lg:col-span-3"></div>
+      <div className="col-start-2 col-span-10 w-full bg-white/15 backdrop-blur-md min-h-50 rounded-3xl border border-slate-800 hover:border-indigo-800 transition lg:row-start-1 lg:col-start-10 lg:col-span-3 "></div>
     </div>
   );
 }
